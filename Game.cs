99fb373f46196571b24282207de0990f740a75c4
Jordan Goddard <jordangoddard6@gmail.com>
2025-01-17 11:14:15 -0700

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Mission1
+{
+    internal class Game
+    {
+        // Method to simulate all dice rolls and return results in an array
+        public int[] AllRolls(int rolls)
+        {
+            // Create new instances of Random class for each die
+            Random die1 = new Random();
+            Random die2 = new Random();
+
+            // Create new array to store instances of each possible dice roll
+            int[] results_array = new int[11];
+
+            // Loop through each dice roll and add results in results_array
+            for (int i = 0; i < rolls; i++)
+            {
+                int result = die1.Next(1, 7) + die2.Next(1, 7);
+
+                if (result == 2)
+                {
+                    results_array[0] += 1;
+                }
+                else if (result == 3)
+                {
+                    results_array[1] += 1;
+                }
+                else if (result == 4)
+                {
+                    results_array[2] += 1;
+                }
+                else if (result == 5)
+                {
+                    results_array[3] += 1;
+                }
+                else if (result == 6)
+                {
+                    results_array[4] += 1;
+                }
+                else if (result == 7)
+                {
+                    results_array[5] += 1;
+                }
+                else if (result == 8)
+                {
+                    results_array[6] += 1;
+                }
+                else if (result == 9)
+                {
+                    results_array[7] += 1;
+                }
+                else if (result == 10)
+                {
+                    results_array[8] += 1;
+                }
+                else if (result == 11)
+                {
+                    results_array[9] += 1;
+                }
+                else if (result == 12)
+                {
+                    results_array[10] += 1;
+                }
+            }
+
+            // Return results_array
+            return results_array;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Mission1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {   // Intro text
+            Console.WriteLine("Welcome to the dice throwing simulator!\n");
+
+            // Ask user to input desired number of dice rolls and store to rolls variable
+            Console.Write("How many dice rolls would you like to simulate? ");
+            int rolls = int.Parse(Console.ReadLine());
+
+            // Create new instance of Game class
+            Game game = new Game();
+
+            // Call AllRolls method from Game class and store results in results_array
+            int[] results_array = game.AllRolls(rolls);
+
+            // Create new array to store percentage of each number rolled
+            int[] number_percentage = new int[11];
+
+            // Calculate percentage of each number rolled and store in number_percentage array
+            for (int i = 0; i < 11; i++)
+            {
+                float rawPercentage = (float)results_array[i] / rolls * 100;
+                number_percentage[i] = (int)Math.Round(rawPercentage);
+            }
+
+            // Display results header
+            Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + rolls);
+
+            // Display results in histogram
+            for (int i = 0; i < 11; i++)
+            {
+                Console.Write("\n" + (i + 2) + ": ");
+                for (int j = 0; j < number_percentage[i]; j++)
+                {
+                    Console.Write("*");
+                }
+            }
+
+            // Closing statement
+            Console.WriteLine("\n\nThank you for using the dice throwing simulator. Goodbye!");
+        }
+    }
+}
